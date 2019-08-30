58dbe1b1aee54a7175e1c6921dbaef51363a9b62
Roxana Popescu <roxana@wolfpack-digital.com>
2019-08-30 12:38:24 +0300

    feed layout complete2

---
+++ src/components/FacebookWall.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="facebookWall">
+    Hello there :)
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FacebookWall'
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../scss/wall"
+</style>
